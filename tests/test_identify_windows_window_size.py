87e4305557e593497922281cb5f89f03ffe14c53
Robert Campbell <h.campbell.robert@gmail.com>
2025-06-17 23:03:22 +0100

    Add window size tests for identify_windows

---
+++ tests/test_identify_windows_window_size.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from portfolio.core import identify_windows
+
+
+def test_identify_windows_various_sizes():
+    df = pd.DataFrame({'x': [0, 0, 0, 0]})
+
+    assert identify_windows(df, 1) == [[0, 1], [1, 2], [2, 3]]
+    assert identify_windows(df, 2) == [[0, 2], [1, 3]]
+    assert identify_windows(df, 3) == [[0, 3]]
