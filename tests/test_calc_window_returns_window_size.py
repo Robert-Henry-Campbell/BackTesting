2d5a8ea4e06f19e96597cc089bf12c3e2c805386
Robert Campbell <h.campbell.robert@gmail.com>
2025-06-18 22:12:07 +0100

    Add window size tests for calc_window_returns

--- tests/test_calc_window_returns_window_size.py
+++ tests/test_calc_window_returns_window_size.py
@@ -1,38 +1,41 @@
 import pandas as pd
 from pandas.testing import assert_frame_equal
 
-from portfolio import calc_window_returns
+from portfolio.core import calc_window_returns
 
 
 def test_calc_window_returns_multiple_window_sizes():
-    df = pd.DataFrame({
-        'date': ['day1', 'day2', 'day3'],
-        'portfolio1': [100, 200, 400],
-    })
+    df = pd.DataFrame(
+        {
+            "date": ["day1", "day2", "day3"],
+            "portfolio1": [100, 200, 400],
+        }
+    )
 
-    # window size 1
     out1 = calc_window_returns(
         df,
         window_size=1,
-        date_column='date',
-        portfolio_columns=['portfolio1'],
+        date_column="date",
+        portfolio_columns=["portfolio1"],
     )
-    expected1 = pd.DataFrame({
-        'window_dates': [['day1', 'day2'], ['day2', 'day3']],
-        'portfolio1_returns': [2.0, 2.0],
-    })
+    expected1 = pd.DataFrame(
+        {
+            "window_dates": [["day1", "day2"], ["day2", "day3"]],
+            "portfolio1_returns": [2.0, 2.0],
+        }
+    )
     assert_frame_equal(out1, expected1, check_dtype=False)
 
-    # window size 2
     out2 = calc_window_returns(
         df,
         window_size=2,
-        date_column='date',
-        portfolio_columns=['portfolio1'],
+        date_column="date",
+        portfolio_columns=["portfolio1"],
     )
-    expected2 = pd.DataFrame({
-        'window_dates': [['day1', 'day3']],
-        'portfolio1_returns': [4.0],
-    })
+    expected2 = pd.DataFrame(
+        {
+            "window_dates": [["day1", "day3"]],
+            "portfolio1_returns": [4.0],
+        }
+    )
     assert_frame_equal(out2, expected2, check_dtype=False)
-
